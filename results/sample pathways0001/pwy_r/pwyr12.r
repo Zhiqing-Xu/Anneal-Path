f44dd028f43b371edd42ba9685484c27ff31c53b
Zhiqing Xu <palladium.xu@gmail.com>
2021-10-22 14:29:27 -0400

    Add files via upload

---
+++ results/sample pathways0001/pwy_r/pwyr12.r
@@ -0,0 +1,35 @@
+if (!requireNamespace("BiocManager", quietly = TRUE))
+    install.packages("BiocManager")
+BiocManager::install("hyperdraw")
+BiocManager::install("hypergraph")
+#$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$#
+#$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$#
+library(hypergraph)
+library(hyperdraw)
+
+DHE0000 <- DirectedHyperedge(c("Serine"), c("Pyruvate"), "EC4.3.1_0")
+DHE0001 <- DirectedHyperedge(c("Hydroxypyruvate"), c("Serine"), "EC1.4.1_0")
+DHE0002 <- DirectedHyperedge(c("Aldehyde","Glyoxylate"), c("Hydroxypyruvate"), "EC4.1.2_0")
+DHE0003 <- DirectedHyperedge(c("D-Allose 6-phosphate"), c("Aldehyde","D-Xylose-5-phosphate"), "EC4.1.2_1")
+DHE0004 <- DirectedHyperedge(c("D-Allose 6-phosphate"), c("Glyoxylate","D-Xylose-5-phosphate"), "EC4.1.2_2")
+DHE0005 <- DirectedHyperedge(c("D-Glucose 6-phosphate"), c("D-Allose 6-phosphate"), "EC1.1.1_0")
+Cnodes <- c("Serine","Pyruvate","Hydroxypyruvate","Aldehyde","Glyoxylate","D-Allose 6-phosphate","D-Xylose-5-phosphate","D-Glucose 6-phosphate")
+Rnodes <- list(DHE0000,DHE0001,DHE0002,DHE0003,DHE0004,DHE0005)
+hg <- Hypergraph(Cnodes,Rnodes)
+hgbph <- graphBPH(hg)
+testrabph <- graphLayout(hgbph)
+edgeDataDefaults(testrabph, "lwd") <- 1
+edgeDataDefaults(testrabph, "color") <- "black"
+nodeDataDefaults(testrabph, "margin") <- 'unit(1, "mm")'
+nodeDataDefaults(testrabph, "shape") <- "box"
+nodeDataDefaults(testrabph, "color") <- "black"
+#plot(testrabph)
+initial.options <- commandArgs(trailingOnly = FALSE)
+file.arg.name <- "--file="
+script.name <- sub(file.arg.name, "", initial.options[grep(file.arg.name, initial.options)])
+script.basename <- dirname(script.name)
+other.name <- file.path(script.basename, "")
+save_path<-paste(other.name,"/pathways/pathway","12.png",sep="")
+png(file = save_path, width = 1920, height = 1080, res = 300)
+plot(testrabph)
+dev.off()
---
+++ results/sample pathways0001/pwy_r/pwyr3.r
@@ -0,0 +1,36 @@
+if (!requireNamespace("BiocManager", quietly = TRUE))
+    install.packages("BiocManager")
+BiocManager::install("hyperdraw")
+BiocManager::install("hypergraph")
+#$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$#
+#$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$#
+library(hypergraph)
+library(hyperdraw)
+
+DHE0000 <- DirectedHyperedge(c("Serine"), c("Pyruvate"), "EC4.3.1_0")
+DHE0001 <- DirectedHyperedge(c("Aldehyde","Glycine"), c("Serine"), "EC4.1.2_0")
+DHE0002 <- DirectedHyperedge(c("D-Threose 4-phosphate"), c("Aldehyde","Glyceraldehyde 3-phosphate"), "EC4.1.2_1")
+DHE0003 <- DirectedHyperedge(c("Glyoxylate"), c("Glycine"), "EC1.4.1_0")
+DHE0004 <- DirectedHyperedge(c("D-Allose 6-phosphate"), c("Glyoxylate","D-Xylose-5-phosphate"), "EC4.1.2_2")
+DHE0005 <- DirectedHyperedge(c("D-Allose 6-phosphate"), c("D-Threose 4-phosphate","Glycolaldehyde"), "EC4.1.2_3")
+DHE0006 <- DirectedHyperedge(c("D-Glucose 6-phosphate"), c("D-Allose 6-phosphate"), "EC1.1.1_0")
+Cnodes <- c("Serine","Pyruvate","Aldehyde","Glycine","D-Threose 4-phosphate","Glyceraldehyde 3-phosphate","Glyoxylate","D-Allose 6-phosphate","D-Xylose-5-phosphate","Glycolaldehyde","D-Glucose 6-phosphate")
+Rnodes <- list(DHE0000,DHE0001,DHE0002,DHE0003,DHE0004,DHE0005,DHE0006)
+hg <- Hypergraph(Cnodes,Rnodes)
+hgbph <- graphBPH(hg)
+testrabph <- graphLayout(hgbph)
+edgeDataDefaults(testrabph, "lwd") <- 1
+edgeDataDefaults(testrabph, "color") <- "black"
+nodeDataDefaults(testrabph, "margin") <- 'unit(1, "mm")'
+nodeDataDefaults(testrabph, "shape") <- "box"
+nodeDataDefaults(testrabph, "color") <- "black"
+#plot(testrabph)
+initial.options <- commandArgs(trailingOnly = FALSE)
+file.arg.name <- "--file="
+script.name <- sub(file.arg.name, "", initial.options[grep(file.arg.name, initial.options)])
+script.basename <- dirname(script.name)
+other.name <- file.path(script.basename, "")
+save_path<-paste(other.name,"/pathways/pathway","3.png",sep="")
+png(file = save_path, width = 1920, height = 1080, res = 300)
+plot(testrabph)
+dev.off()
